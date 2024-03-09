81b7ed33860ef397c26e8c05fefa6c6f6938ff12
Niloyan Sellathurai <124621796+NiloyanSellathurai02@users.noreply.github.com>
2024-03-09 01:33:11 +0100

    I finished the app and everything works fine

---
+++ src/components/Results.jsx
@@ -0,0 +1,41 @@
+import {calculateInvestmentResults, formatter} from "../util/investment";
+
+export default function Results({input}) {
+  const resultsData = calculateInvestmentResults(input);
+  const initialInvestment = resultsData[0].valueEndOfYear - resultsData[0].interest - resultsData[0].annualInvestment;
+
+  console.log(resultsData);
+
+  return (
+    <>
+      <table id="result">
+        <thead>
+          <tr>
+            <th>Year</th>
+            <th>Investement Value</th>
+            <th>Interest (Year)</th>
+            <th>Total Investment</th>
+            <th>Invested Capital</th>
+          </tr>
+        </thead>
+        <tbody>
+          {resultsData.map((yearData) => {
+            const totalIntrest =
+              yearData.valueEndOfYear - yearData.annualInvestment * yearData.year - initialInvestment;
+
+            const totalAmountInvested = yearData.valueEndOfYear - totalIntrest;
+            return (
+              <tr key={yearData.year}>
+                <td>{yearData.year}</td>
+                <td>{formatter.format(yearData.valueEndOfYear)}</td>
+                <td>{formatter.format(yearData.interest)}</td>
+                <td>{formatter.format(totalIntrest)}</td>
+                <td>{formatter.format(totalAmountInvested)}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
+}
